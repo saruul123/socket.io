a99f62249d6558d21115fe488cace8158950955f
saruul123 <saruulerdem2018@gmail.com>
2022-11-15 14:29:13 +0800

    socket riin

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+const adminNameSpace = io.of('/admin')
+
+adminNameSpace.on('connect', (socket) => {
+    // console.log('a user connected');
+
+    socket.on('join', (data) =>
+    {
+        socket.join(data.room)
+        adminNameSpace.in(data.room).emit('chat message', `New Person joined the ${data.room} room`)
+    })
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('chat message', (data) => {
+        adminNameSpace.in(data.room).emit('chat message', data.msg);
     });
 });
 
