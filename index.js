a40b4a52f5a6c8fb34bee6f4d2c1bbea53791932
saruul123 <saruulerdem2018@gmail.com>
2022-11-15 15:58:48 +0800

    create rooms

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const roomList = []
 
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/rooms', (req, res) => {
-    res.sendFile(__dirname + '/rooms.html');
+    res.render(path.join(__dirname,'/','index.html'),{rooms: roomList})
 });
 
 app.get('/executive', (req, res) => {
@@ -21,6 +21,18 @@ app.get('/engineer', (req, res) => {
     res.sendFile(__dirname + '/engineer.html');
 });
 
+app.get('/room/', (req, res) => {
+    var name = req.query.name
+    res.render(path.join(__dirname,'/','rooms.html'),{rooms: name})
+});
+
+app.get('/addRoom/', (req, res) =>
+{
+    var name =req.query.name
+    roomList.push(name)
+    console.log(JSON.stringify(roomList));
+    res.send(200)
+})
 
 const adminNameSpace = io.of('/admin')
 
